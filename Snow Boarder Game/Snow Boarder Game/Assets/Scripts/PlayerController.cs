f1f105fd0c567e9b6b100092ad5e5c6a4a26048b
Alex Niunko <138675683+svindom@users.noreply.github.com>
2023-12-07 22:42:03 +0200

    Added speed up booster to the player and made map bigger

--- Snow Boarder Game/Snow Boarder Game/Assets/Scripts/PlayerController.cs
+++ Snow Boarder Game/Snow Boarder Game/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _raynevan;
+    SurfaceEffector2D _surfaceEffector;
+
     public float torqueAmount = 1f;
+    public float boostSpeed = 20f;
+    private float _speed = 14f;
 
     private void Awake()
     {
         _raynevan = GetComponent<Rigidbody2D>();
+        _surfaceEffector = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
     void Start()
@@ -17,11 +22,13 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         RotateRaynevan(torqueAmount);
+        BoostSpeed();
     }
+
+
 
     public void RotateRaynevan(float torque)
     {
@@ -33,5 +40,18 @@ public class PlayerController : MonoBehaviour
         {
             _raynevan.AddTorque(-torque);
         }
+    }
+
+    private void BoostSpeed()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _surfaceEffector.speed = boostSpeed;
+        }
+        else
+        {
+            _surfaceEffector.speed = _speed;
+        }
+        
     }
 }
